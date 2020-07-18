f42bc7e8e9bde5ad242cc515d9d7abb0d208e833
Vaishnav Potlapalli <51985757+va1shn9v@users.noreply.github.com>
2020-07-19 03:29:42 +0530

    Update train_net_step.py

--- tools/train_net_step.py
+++ tools/train_net_step.py
@@ -158,8 +158,8 @@ def main():
         raise ValueError("Need number of classes in your custom dataset to run!")
 
     if args.dataset == "coco2017":
-        cfg.TRAIN.DATASETS = ('coco_2017_train',)
-        cfg.MODEL.NUM_CLASSES = 81
+        cfg.TRAIN.DATASETS = ('coco_2014_train',)
+        cfg.MODEL.NUM_CLASSES = 4
     elif args.dataset == "keypoints_coco2017":
         cfg.TRAIN.DATASETS = ('keypoints_coco_2017_train',)
         cfg.MODEL.NUM_CLASSES = 2
